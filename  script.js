619686ae9994a431d288b489474dfede7a8117ed
Susila Chapagain <susilachapagain@192-168-1-113.tpgi.com.au>
2022-01-20 23:17:03 +1100

    first commit

---
+++  script.js
@@ -0,0 +1,6 @@
+let searchBtn= document.querySelector('#search-btn');
+let searchBtn= document.querySelector('#search-bar-container'); 
+
+searchBtn.addEventListener('click',() =>{
+    searchBtn.classList.toggle('fa-times');
+});
